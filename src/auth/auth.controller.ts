0256859c7bcbc1c20f380403332f8274ff4cd2ca
Jerome Fabricante <jerome.fabricante@opsolutions.biz>
2022-06-26 10:06:26 +0800

    Auth and Users Module Add LocalStrategy and LocalAuthGuard

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    async login(@Request() req) {
+        return req.user;
+    }
+}
