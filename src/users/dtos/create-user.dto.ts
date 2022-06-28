1595a8fe36b38087f7c3a01e307bc632ca0602f4
Jerome Fabricante <jerome.fabricante@opsolutions.biz>
2022-06-28 21:27:26 +0800

    Create user dto

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  role: string;
+
+  @IsString()
+  displayName: string;
+}
