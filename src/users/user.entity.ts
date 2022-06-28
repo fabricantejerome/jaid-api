5eb994f936176aba888d643d8580871223adb2da
Jerome Fabricante <jerome.fabricante@opsolutions.biz>
2022-06-28 21:27:16 +0800

    Create entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,42 @@
+import {
+    AfterInsert,
+    AfterRemove,
+    AfterUpdate,
+    Entity,
+    Column,
+    PrimaryGeneratedColumn
+} from 'typeorm';
+  
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+  
+    @Column()
+    password: string;
+  
+    @Column({ default: true })
+    role: string;
+
+    @Column()
+    displayName: string;
+  
+    @AfterInsert()
+    logInsert() {
+      console.log('Inserted User with id', this.id);
+    }
+  
+    @AfterUpdate()
+    logUpdate() {
+      console.log('Updated User with id', this.id);
+    }
+  
+    @AfterRemove()
+    logRemove() {
+      console.log('Removed User with id', this.id);
+    }
+}
+  
